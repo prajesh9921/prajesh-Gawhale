67e878a15d423d709167a6be8f3ddcdac30a1c84
prajesh9921 <prajeshgawhale2019.it@mmcoe.edu.in>
2024-02-13 11:42:20 +0530

    fixed lottie issue

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./header.css";
 import CTA from "./CTA";
 import HeaderSocial from "./headerSocial";
 import ComputerAnimation from "../../assets/lottie/computer.json";
 import Lottie from "react-lottie";
 
-const header = () => {
-  // const mediaQuery = window.matchMedia("(max-width: 600px)");
+const Header = () => {
+
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const heightWeight = windowSize/3; 
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
 
   const defaultOptions = {
     loop: true,
@@ -19,21 +33,21 @@ const header = () => {
 
   return (
     <header data-section id="header">
-      <div className="container header_container">
+      <div className="header_container">
         <h5>Hello I'm</h5>
         <h1 className="header_name">Prajesh Gawhale</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
-        <HeaderSocial />
-        <div className="me">
-          <Lottie options={defaultOptions} />
+        <div className="socials">
+          <HeaderSocial />
+            <Lottie options={defaultOptions} height={windowSize <= 480 ? 250 : heightWeight} width={windowSize <= 480 ? 250 : heightWeight}/>
+          <a href="#contact" className="scroll_down">
+            Scroll Down
+          </a>
         </div>
-        <a href="#contact" className="scroll_down">
-          Scroll Down
-        </a>
       </div>
     </header>
   );
 };
 
-export default header;
+export default Header;
